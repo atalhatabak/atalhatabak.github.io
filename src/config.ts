784143f57ef2e33fd3a316f4f9d466c55f9414e0
atalhatabak <atalhatabak@gmail.com>
2024-04-07 13:55:15 +0300

    make for github pages

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Talha Tabak | Kişisel Tanıtım Sitesi';
+export const SITE_DESCRIPTION = 'Talha Tabak';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
